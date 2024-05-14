833cecd1a6a4bc24dbcc79fe85e041985cc39ae1
Michael Reyesgit config --global user.email michael4reyes7@gmail.comgit statusclearclear <michael4reyes7@gmail.com>
2024-05-13 21:45:51 -0700

    add basic validation to email input, in progress

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 const emailInput = document.querySelector('input');
-console.log(emailInput);
+const subscribeButton = document.getElementById('subscribe');
+
+const emailValidation = () => {
+
+    if (emailInput.value.length > 0) {
+         return true;
+    }
+    else {
+        return false;
+    };
+}
+subscribeButton.addEventListener('click', () => {
+    const articleCard = document.querySelector('article');
+    const successMessage = document.querySelector('.success');
+    if (emailValidation) {
+        articleCard.classList.add('hidden');
+        successMessage.classList.remove('hidden');
+    } else return;
+
+})
