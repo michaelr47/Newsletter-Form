5820400189cdfce308b74acf026fe71205c2260a
Michael Reyesgit config --global user.email michael4reyes7@gmail.comgit statusclearclear <michael4reyes7@gmail.com>
2024-05-14 21:15:56 -0700

    add email validation using regex

--- index.js
+++ index.js
@@ -2,18 +2,16 @@ const emailInput = document.querySelector('input');
 const subscribeButton = document.getElementById('subscribe');
 
 const emailValidation = () => {
-
-    if (emailInput.value.length > 0) {
-         return true;
-    }
-    else {
-        return false;
-    };
+    const regex = /^[a-zA-Z0–9._-]+@[a-zA-Z0–9.-]+\.[a-zA-Z]{2,4}$/;
+    return regex.test(emailInput.value);
 }
+
+
+
 subscribeButton.addEventListener('click', () => {
     const articleCard = document.querySelector('article');
     const successMessage = document.querySelector('.success');
-    if (emailValidation) {
+    if (emailValidation()) {
         articleCard.classList.add('hidden');
         successMessage.classList.remove('hidden');
     } else return;
