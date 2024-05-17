55120bf8b158c59f1e0f57683ac41b60867471d9
Michael Reyesgit config --global user.email michael4reyes7@gmail.comgit statusclearclear <michael4reyes7@gmail.com>
2024-05-16 18:43:48 -0700

    add red placeholder text upon wrong inpu

--- index.js
+++ index.js
@@ -11,11 +11,14 @@ const emailValidation = () => {
 subscribeButton.addEventListener('click', () => {
     const articleCard = document.querySelector('article');
     const successMessage = document.querySelector('.success');
+    const errorMessage = document.querySelector('.errorMessage');
     if (emailValidation()) {
         articleCard.classList.add('hidden');
         successMessage.classList.remove('hidden');
     } else {
-        emailInput.placeholder = 'ash#loremcompany.com'
+        emailInput.placeholder = 'ash#loremcompany.com';
+        emailInput.classList.add('errorPlaceholder');
+        errorMessage.classList.remove('hidden');
     };
 
 })
